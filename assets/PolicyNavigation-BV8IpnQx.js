595bf906af5d6d02fe51d619a29145fd33485eec
dhruvshivhare1 <dhruvshivhare16@gmail.com>
2025-06-03 04:23:33 +0530

    Add files via upload

---
+++ assets/PolicyNavigation-BV8IpnQx.js
@@ -0,0 +1,6 @@
+import{c as s,j as e,L as t}from"./index-Nzu8vXX0.js";/**
+ * @license lucide-react v0.344.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const a=s("ArrowLeft",[["path",{d:"m12 19-7-7 7-7",key:"1l729n"}],["path",{d:"M19 12H5",key:"x3x0zl"}]]),o=()=>e.jsx("nav",{className:"bg-white shadow-sm fixed top-0 w-full z-50",children:e.jsx("div",{className:"container mx-auto px-4 md:px-6",children:e.jsx("div",{className:"flex items-center h-16",children:e.jsxs(t,{to:"/",className:"flex items-center text-gray-600 hover:text-secondary-600 transition-colors",children:[e.jsx(a,{size:20,className:"mr-2"}),"Back to Home"]})})})});export{o as P};
